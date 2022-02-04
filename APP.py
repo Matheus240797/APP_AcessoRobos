aeeedc8c74f50ad14a3c33353528f19cbe1e7861
Matheus Amaral Miranda <matheus.amaral@akrkpromotora.local>
2022-02-04 09:44:46 -0300

    Atualização APP.py

--- APP.py
+++ APP.py
@@ -14,14 +14,14 @@ print("")
 print("Robôs:")
 
 if cod_ambiente == 1:
-#CONSULTA LISTA DE ROBÔS 
+#CONSULTA LISTA DE ROBÔS - (AMBIENTE 1)
     i = 1
     while i < len(lst_robos_producao):
         print(lst_robos_producao[i][2],"-", lst_robos_producao[i][0])
         i = i + 1
 
 if cod_ambiente == 2:
-#CONSULTA LISTA DE ROBÔS 
+#CONSULTA LISTA DE ROBÔS - (AMBIENTE 2)
     i = 1
     while i < len(lst_robos_homologa):
         print(lst_robos_homologa[i][2],"-", lst_robos_homologa[i][0])
